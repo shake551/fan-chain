b801a4b74b6a93bfb4cdf93b6078c53c528165df
daikiando <daikiando@andoutaikinoMacBook-Pro.local>
2023-08-19 02:14:00 +0900

    cardのスタイル調整

--- src/components/Votes.tsx
+++ src/components/Votes.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Proposal, ProposalState } from '@thirdweb-dev/sdk' // 必要なSDKのインポートを追加
+import { Proposal, ProposalState } from '@thirdweb-dev/sdk'
 import { useContract, useContractRead } from '@thirdweb-dev/react'
 import { VoteCard } from './card/VoteCard'
 import { Box, Flex } from '@chakra-ui/react'
@@ -8,7 +8,7 @@ interface VotesProps {
   targetState: ProposalState; 
 }
 
-export function Votes({ targetState }: VotesProps) { // VotesPropsを指定
+export function Votes({ targetState }: VotesProps) { 
   const { contract } = useContract(process.env.NEXT_PUBLIC_CONTRACT_ADDRESS)
 
   const { data: proposals, isLoading } = useContractRead(contract, 'getAllProposals')
