cd2ee699e8902d008ce0288a1321e73767138c67
daikiando <daikiando@andoutaikinoMacBook-Pro.local>
2023-08-19 03:50:49 +0900

    ProposalStateNameのバッジ化とDividerで横線追加

--- src/components/Votes.tsx
+++ src/components/Votes.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Proposal, ProposalState } from '@thirdweb-dev/sdk';
 import { useContract, useContractRead } from '@thirdweb-dev/react';
-import { Box, Flex } from '@chakra-ui/react';
+import { Box, Flex, Badge, Divider } from '@chakra-ui/react';
 import { VoteCard } from './card/VoteCard';
+import { ProposalStateName } from '@/utils/proposalState';
 
 interface VotesProps {
   targetState: ProposalState; 
@@ -15,16 +16,29 @@ export function Votes({ targetState }: VotesProps) {
 
   return (
     <div>
-      <h1>Vote Proposals</h1>
+      
+      <Divider marginBottom='20px' borderColor='black'/> {/* 横線のカラーを指定 */}
+      <Flex alignItems='flex-end' marginBottom='10px'>
+        <Badge
+          colorScheme='gray'
+          fontSize='sm'
+          p={2}
+          borderRadius='md'
+          boxShadow='md'
+          bg='gray.100'
+        >
+          {ProposalStateName(targetState)}
+        </Badge>
+      </Flex>
       {isLoading ? (
         <p>Loading...</p>
       ) : (
-        <Flex direction='row' flexWrap='wrap' width='1500px'> {/* 全体の幅を一定に保つ */}
+        <Flex direction='row' flexWrap='wrap' width='1500px'>
           {proposals?.map((proposal: Proposal, i: number) => (
             <Box
               key={i}
-              width='300px' // 各カードの幅を一定に設定
-              margin='30px' // カードの間隔
+              width='300px'
+              margin='30px'
             >
               <VoteCard proposal={proposal} targetState={targetState} />
             </Box>
